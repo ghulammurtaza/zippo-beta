454bf5ffcddc44783459fc77b9ff480553151389
Murtaza <Murtaza@lenovo>
2014-09-26 01:10:13 +0500

    fixed jquery conflicts and addtocart isue

--- skin/frontend/zippo/default/js/responsive.js
+++ skin/frontend/zippo/default/js/responsive.js
@@ -1,21 +1,13 @@
-$(document).ready(function(e) {
+jQuery(document).ready(function(e) {
 	
-if($(window).width() <= 767){
+if(jQuery(window).width() <= 767){
   
-$(".globalNav ").append("<button id='menu' type='button'> <span class='icon-bar'></span> <span class='icon-bar'></span> <span class='icon-bar'></span> </button>"); 	
+jQuery(".globalNav ").append("<button id='menu' type='button'> <span class='icon-bar'></span> <span class='icon-bar'></span> <span class='icon-bar'></span> </button>"); 	
 		
-	$("#menu").click(function() {
+	jQuery("#menu").click(function() {
 		//alert("hello");
-    	$("#catTopNav").slideToggle();
-		$("#container").toggleClass("layer");    
+    	jQuery("#catTopNav").slideToggle();
+		jQuery("#container").toggleClass("layer");    
     });
-	
-	//$(".genTopNav").prepend("<a>Navigation</a>")
-	
 }
-
-
-
-
-
 });
