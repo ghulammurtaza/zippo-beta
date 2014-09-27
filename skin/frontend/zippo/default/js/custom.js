8eeadca0fcb6641c6e8835a06362c71b5605d499
Naveed Ahmed <frenklen2004@gmail.com>
2014-09-27 19:35:05 +0400

    test

---
+++ skin/frontend/zippo/default/js/custom.js
@@ -0,0 +1,31 @@
+jQuery(function()
+{
+    jQuery('.scroll-pane-before').jScrollPane(
+            {
+                showArrows: true,
+                verticalArrowPositions: 'before',
+                horizontalArrowPositions: 'before'
+            }
+    );
+    jQuery('.scroll-pane-after').jScrollPane(
+            {
+                showArrows: true,
+                verticalArrowPositions: 'after',
+                horizontalArrowPositions: 'after'
+            }
+    );
+    jQuery('.scroll-pane-os').jScrollPane(
+            {
+                showArrows: true,
+                verticalArrowPositions: 'os',
+                horizontalArrowPositions: 'os'
+            }
+    );
+    jQuery('.scroll-pane-split').jScrollPane(
+            {
+                showArrows: true,
+                verticalArrowPositions: 'split',
+                horizontalArrowPositions: 'split'
+            }
+    );
+});
