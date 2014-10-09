6f7e6e2755bee95567d275d9d46df2529fba08d4
Naveed Ahmed <frenklen2004@gmail.com>
2014-10-09 19:57:36 +0400

    Skin Updated

--- skin/frontend/zippo/default/js/custom.js
+++ skin/frontend/zippo/default/js/custom.js
@@ -30,4 +30,41 @@ jQuery(function()
     );
 });
 
+function test (){
+    jQuery(".products-grid > li a").mouseover(function(e) {
+       jQuery(this).parents(".products-grid > li").addClass("popupActive");
+    });
+	jQuery(".listProdPopUps").mouseleave(function(e) {
+		 jQuery(".products-grid > li").removeClass("popupActive");
+		 jQuery(".products-grid > li .listProdPopUps").hide();
+    });
+	jQuery(".products-grid > li").mouseleave(function(e) {
+		 jQuery(".products-grid > li").removeClass("popupActive");
+		 jQuery(".products-grid > li .listProdPopUps").hide();
+    });
+	jQuery(".products-grid > li .btnAddCart").click(function(e) {
+		jQuery(".productListingPopup .prodCart").stop();
+		jQuery(".productListingPopup .prodCart").append("<span class='loader-cart'></span>");
+		setTimeout(function(){
+			jQuery(".productListingPopup .prodCart span").remove();
+			jQuery("html").clearQueue();
+			jQuery(".productListingPopup .prodCart").clearQueue();
+     	 	jQuery(".products-grid > li").removeClass('popupActive');
+			jQuery("html").animate({scrollTop: 0}, 500);
+			
+			return false;
+		 
+		 }, 4000);
+		
+    });
 
+
+	
+}
+
+jQuery(document).ready(function(e) {
+  test(); 
+});
+jQuery(window).scroll(function(e) {
+  test();
+});
