c509aa1a489062196a0d7b3d29d063674e630f7c
ghulammurtaza <murtaza.gondal@gmail.com>
2014-11-04 12:32:13 +0500

    check to stop checkout with out gift code

--- app/code/local/Zippo/Search/Model/Search.php
+++ app/code/local/Zippo/Search/Model/Search.php
@@ -3,6 +3,7 @@
 class Zippo_Search_Model_Search extends Mage_Core_Model_Abstract {
 
     protected $_searchCriteria = array();
+    protected $_cmsSearchKey ;
 
     function setSearchCriteria($data) {
         $this->_searchCriteria = $data;
@@ -10,5 +11,13 @@ class Zippo_Search_Model_Search extends Mage_Core_Model_Abstract {
 
     function getSearchCriteria() {
         return $this->_searchCriteria;
+    }
+    
+    function setCmsSearchKey($keyword) {
+        $this->_cmsSearchKey = $keyword;
+    }
+
+    function getCmsSearchKey() {
+        return $this->_cmsSearchKey;
     }
 }
