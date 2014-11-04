6187a4c23f6c470eaf216a72e401c41308e7b4f6
ghulammurtaza <murtaza.gondal@gmail.com>
2014-11-04 16:53:04 +0500

    check to stop checkout with out gift code

--- app/code/local/Zippo/Search/Block/Cms.php
+++ app/code/local/Zippo/Search/Block/Cms.php
@@ -1,22 +1,29 @@
 <?php
-class Zippo_Search_Block_Cms extends Mage_Core_Block_Template 
-{
-    protected $_cmsCollection;
-    
-    protected function _getCmsCollection()
-    {
-        if (is_null($this->_cmsCollection)) {
-             $searchKey = Mage::getSingleton('zippo_search/search')->getCmsSearchKey();
-             
-             
-             $this->_cmsCollection = $collection;                           
-        }
 
-        return $this->_cmsCollection;
+class Zippo_Search_Block_Cms extends Mage_Core_Block_Template {
+
+    protected function _getCmsCollection() {
+
+        $keyword = Mage::getSingleton('zippo_search/search')->getCmsSearchKey();
+        $collection = Mage::getModel('cms/page')->getCollection()
+                ->addFieldToFilter("is_active", 1)
+                ->addFieldToFilter(
+                        array('identifier', 'title', 'content', 'content_heading', 'meta_keywords', 'meta_description'), array(
+                            array('like' => '%' . $keyword . '%'),
+                            array('like' => '%' . $keyword . '%'),
+                            array('like' => '%' . $keyword . '%'),
+                            array('like' => '%' . $keyword . '%'),
+                            array('like' => '%' . $keyword . '%'),
+                            array('like' => '%' . $keyword . '%'),
+                        )
+                )
+                ->setCurPage(1)
+                ->setOrder('title', 'ASC');
+        return $collection->load();
     }
-    
-    public function getCmsCollection()
-    {
+
+    public function getCmsCollection() {
         return $this->_getCmsCollection();
     }
+
 }
