89f7bc47d03df2fa254c70722e530759f71a68a0
Naveed Ahmed <frenklen2004@gmail.com>
2014-12-19 18:39:43 +0400

    Updated Skin

--- skin/frontend/zippo/default2/js/custom.js
+++ skin/frontend/zippo/default2/js/custom.js
@@ -30,7 +30,7 @@ jQuery(function()
     );
 });
 
-function test (){
+function popUp (){
 
 	jQuery(".listProdPopUps").mouseleave(function(e) {
 		 jQuery(".products-grid > li").removeClass("popupActive");
@@ -47,11 +47,11 @@ function test (){
 	
 		
 		jQuery(".productListingPopup .prodCart").append("<span class='loader-cart'></span>");
-		var appndMsg = jQuery(this).parents(".inner")
+		//var appndMsg = jQuery(".productListingPopup .succcessMsg");
 		setTimeout(function(){
 			
 			jQuery(".productListingPopup .prodCart span").remove();
-			appndMsg.append("<div class='addedincart'>successfully added to your cart</div>");
+			jQuery(".productListingPopup .succcessMsg").append("<div class='addedincart'>successfully added to your cart</div>");
 
 
 			//jQuery("html").animate({scrollTop: 0}, 500);
@@ -146,16 +146,23 @@ jQuery(".productListingPopup .inner").clearQueue();
 
 jQuery(document).ready(function(e) {
 		
-  jQuery('.products-grid > li').mousestop(1000, function() {
+  jQuery('.products-grid > li').mousestop(300, function() {
 		jQuery(this).addClass("popupActive");
-		test();
+		popUp();
 		
 });
   // 
 });
 jQuery(window).scroll(function(e) {
-   jQuery('.products-grid > li').mousestop(1000, function() {
+   jQuery('.products-grid > li').mousestop(300, function() {
 		jQuery(this).addClass("popupActive");
-		test();
+		popUp();
 });
 });
+
+function addtocart(id, url) {
+        qty = jQuery('#txtProdQty' + id).val();
+        url = url + '&qty=' + qty;
+        setLocation(url);
+        return false;
+    }
